30b81b31ec9bd6eba8eaf75df1a0422df0a3dab3
Veronika CHERNAIA <Veronika.chernaia75@gmail.com>
2024-05-07 14:48:20 +0000

    operational

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,10 +1,16 @@
-SELECT 
-  date_date, 
-  orders_id,
-  ship.ship_cost,
-  ship.shipping_fee,
-  ship.logcost
-
-FROM {{ref('int_sales_margin')}} AS margin
-JOIN {{ref('stg_raw__product')}} AS ship
-USING (orders_id)
+SELECT
+    o.orders_id
+  ,o.date_date
+  ,ROUND((CAST(o.margin AS FLOAT64) + CAST(s.shipping_fee AS FLOAT64) - CAST(s.logcost AS FLOAT64) - CAST(s.ship_cost AS FLOAT64)),2) AS operational_margin
+  ,o.quantity
+  ,o.revenue
+  ,o.purchase_cost
+  ,o.margin
+  ,s.shipping_fee
+  ,s.logcost
+  ,s.ship_cost
+  
+FROM `quantum-lambda-420409`.`dbt_vchernaia`.`int_sales_margin` AS o
+JOIN `quantum-lambda-420409`.`dbt_vchernaia`.`stg_raw__ship` AS s 
+USING(orders_id)
+ORDER BY o.orders_id desc
